d8d952f19949e3e2fcc99a13e0cbbacd94eeaceb
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2020-10-05 07:23:36 -0500

    Update server.go

--- server.go
+++ server.go
@@ -12,22 +12,22 @@ type user struct {
 }
 
 func (u *user) newMessage(text string) {
-	u.message = append(u.message, string(text))
+	u.message = append(u.message, string(text))//add a new value to a string
 }
 func (u *user) messageRequest(conn net.Conn) {
 
-	defer conn.Close()
+	defer conn.Close()//close the conection
 	defer log.Println("Closed connection.")
 
 	for {
-		io.WriteString(conn, fmt.Sprintln(u.message[0:]))
-		buf := make([]byte, 1024)
+		io.WriteString(conn, fmt.Sprintln(u.message[0:]))//send the string
+		buf := make([]byte, 1024)//read the conection
 		size, err := conn.Read(buf)
 		if err != nil {
-			return
+			return//if err return
 		}
-		data := buf[:size]
-		u.newMessage(string(data))
+		data := buf[:size]//convert
+		u.newMessage(string(data))//add to  the string
 		fmt.Print(u.message)
 
 	}
@@ -36,7 +36,7 @@ func (u *user) messageRequest(conn net.Conn) {
 func main() {
 	message := user{}
 
-	l, err := net.Listen("tcp", ":500")
+	l, err := net.Listen("tcp", ":500")//listen 
 	if err != nil {
 		log.Panicln(err)
 	}
